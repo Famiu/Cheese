f1f5eba4630973c5e1dbcaf0cdf36ef10f03414f
Famiu Haque <famiuhaque@gmail.com>
2020-06-09 11:35:15 +0600

    Initial Commit

---
+++ Cheese.cc
@@ -0,0 +1,7 @@
+#include "xboard_funcs.hh"
+
+int main()
+{
+    // Don't buffer on output
+    std::cout.setf(std::ios::unitbuf);
+}
---
+++ xboard_funcs.hh
@@ -0,0 +1,12 @@
+#ifndef __XBOARD_FUNCS_H_
+#define __XBOARD_FUNCS_H_
+
+#include <iostream>
+#include <string>
+
+bool is_command_available()
+{
+
+}
+
+#endif // __XBOARD_FUNCS_H_
